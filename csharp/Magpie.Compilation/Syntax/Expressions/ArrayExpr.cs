6acb68dd57cd9685baf254e27a7ec5cfdd69d45c
Robert Nystrom <robert@stuffwithstuff.com>
2010-04-02 08:56:21 -0400

    Adding some documentation.

--- csharp/Magpie.Compilation/Syntax/Expressions/ArrayExpr.cs
+++ csharp/Magpie.Compilation/Syntax/Expressions/ArrayExpr.cs
@@ -5,25 +5,52 @@ using System.Text;
 
 namespace Magpie.Compilation
 {
+    /// <summary>
+    /// An array literal expression like [1, 2, 3].
+    /// </summary>
     public class ArrayExpr : IUnboundExpr
     {
+        /// <summary>
+        /// Gets the collection of elements in the array. May be be empty.
+        /// </summary>
         public readonly List<IUnboundExpr> Elements = new List<IUnboundExpr>();
+
+        /// <summary>
+        /// Gets the array element type. All elements of an array must be the same type.
+        /// </summary>
         public IUnboundDecl ElementType { get { return mElementType; } }
 
+        /// <summary>
+        /// Gets the position of this expression in its source file.
+        /// </summary>
         public Position Position { get; private set; }
 
+        /// <summary>
+        /// Instantiates a new instance of ArrayExpr.
+        /// </summary>
+        /// <param name="position">Where the expression occurs in the source file.</param>
+        /// <param name="elements">The array elements.</param>
         public ArrayExpr(Position position, IEnumerable<IUnboundExpr> elements)
         {
             Position = position;
             Elements.AddRange(elements);
         }
 
+        /// <summary>
+        /// Instantiates a new instance of a zero-length array.
+        /// </summary>
+        /// <param name="position">Where the expression occurs in the source file.</param>
+        /// <param name="elementType">The element type. This must be explicitly passed in since
+        /// it cannot be inferred from an element.</param>
         public ArrayExpr(Position position, IUnboundDecl elementType)
         {
             Position = position;
             mElementType = elementType;
         }
 
+        /// <summary>
+        /// Overridden from Object.
+        /// </summary>
         public override string ToString()
         {
             if (Elements.Count == 0)
--- csharp/Magpie.Compilation/Syntax/Expressions/AssignExpr.cs
+++ csharp/Magpie.Compilation/Syntax/Expressions/AssignExpr.cs
@@ -5,17 +5,45 @@ using System.Text;
 
 namespace Magpie.Compilation
 {
+    /// <summary>
+    /// An assignment expression like "foo &lt;- bar"
+    /// </summary>
     public class AssignExpr : IUnboundExpr
     {
+        /// <summary>
+        /// Gets the position of this expression in its source file.
+        /// </summary>
         public Position Position { get; private set; }
+
+        /// <summary>
+        /// Gets the target lvalue of the expression: this what the assignment will assign into.
+        /// </summary>
         public IUnboundExpr Target { get; private set; }
+
+        /// <summary>
+        /// Gets the expression for the value that will be assigned in the expression.
+        /// </summary>
         public IUnboundExpr Value { get; private set; }
 
+        /// <summary>
+        /// Instantiates a new instance of AssignExpr.
+        /// </summary>
+        /// <param name="position">Where the expression occurs in the source file.</param>
+        /// <param name="target">The target of the assignment.</param>
+        /// <param name="value">The value being assigned.</param>
         public AssignExpr(Position position, IUnboundExpr target, IUnboundExpr value)
         {
             Position = position;
             Target = target;
             Value = value;
+        }
+
+        /// <summary>
+        /// Overridden from Object.
+        /// </summary>
+        public override string ToString()
+        {
+            return String.Format("{0} <- {1}", Target, Value);
         }
 
         public TReturn Accept<TReturn>(IUnboundExprVisitor<TReturn> visitor)
@@ -30,7 +58,5 @@ namespace Magpie.Compilation
 
             return transformer.Transform(this);
         }
-
-        public override string ToString() { return String.Format("{0} <- {1}", Target, Value); }
     }
 }
