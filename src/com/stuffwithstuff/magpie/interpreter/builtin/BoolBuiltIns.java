99e354f24398ab2ba953bc4c8cc7f79384843ed7
Robert Nystrom <robert@stuffwithstuff.com>
2010-09-29 08:39:37 -0700

    Starting to write Magpie spec in Magpie.

--- src/com/stuffwithstuff/magpie/interpreter/builtin/BoolBuiltIns.java
+++ src/com/stuffwithstuff/magpie/interpreter/builtin/BoolBuiltIns.java
@@ -4,6 +4,15 @@ import com.stuffwithstuff.magpie.interpreter.Interpreter;
 import com.stuffwithstuff.magpie.interpreter.Obj;
 
 public class BoolBuiltIns {
+  @Signature("==(other Object -> Bool)")
+  public static Obj _eqeq_(Interpreter interpreter, Obj thisObj, Obj arg) {
+    if (arg.getValue() instanceof Boolean) {
+      return interpreter.createBool(thisObj.asBool() == arg.asBool());
+    } else {
+      return interpreter.createBool(false);
+    }
+  }
+
   @Signature("not(-> Bool)")
   public static Obj not(Interpreter interpreter, Obj thisObj, Obj arg) {
     return interpreter.createBool(!thisObj.asBool());
