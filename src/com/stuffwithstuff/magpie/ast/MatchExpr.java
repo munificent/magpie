752f8b16c67c3dbcb42ad84633a519da00a48158
Bob Nystrom <robert@stuffwithstuff.com>
2011-02-03 18:04:38 -0800

    Pretty-printing for match expressions.

--- src/com/stuffwithstuff/magpie/ast/MatchExpr.java
+++ src/com/stuffwithstuff/magpie/ast/MatchExpr.java
@@ -23,7 +23,18 @@ public class MatchExpr extends Expr {
 
   @Override
   public void toString(StringBuilder builder, String indent) {
-    builder.append("not implemented :(");
+    builder.append(indent).append("match ");
+    mValue.toString(builder, indent);
+    builder.append("\n");
+    
+    for (MatchCase matchCase : mCases) {
+      builder.append(indent).append("    case ");
+      builder.append(matchCase.getPattern());
+      builder.append(" then ");
+      matchCase.getBody().toString(builder, indent + "        ");
+    }
+    
+    builder.append(indent).append("end");
   }
 
   private final Expr mValue;
