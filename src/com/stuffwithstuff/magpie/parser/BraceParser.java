54f37cc02120230688d2c188cbaeba6d7a7e341d
Bob Nystrom <robert@stuffwithstuff.com>
2011-02-06 22:46:50 -0800

    Add (unused) syntax for defining operators.
    Can now do:
    
        defoperator +- 123 (left, right) ...
    
    To create an infix operator function.

--- src/com/stuffwithstuff/magpie/parser/BraceParser.java
+++ src/com/stuffwithstuff/magpie/parser/BraceParser.java
@@ -1,15 +1,38 @@
 package com.stuffwithstuff.magpie.parser;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.stuffwithstuff.magpie.ast.Expr;
 
 public class BraceParser extends PrefixParser {
   @Override
   public Expr parse(MagpieParser parser, Token token) {
     parser.pushQuote();
+    
     Position position = token.getPosition();
-    Expr expr = parser.groupExpression(TokenType.RIGHT_BRACE);
+    
+    // Ignore a line after {
+    parser.match(TokenType.LINE);
+    
+    // Parse a sequence of expressions.
+    List<Expr> exprs = new ArrayList<Expr>();
+    while (true) {
+      exprs.add(parser.parseExpression());
+      
+      // Stop when we hit the closing }.
+      if (parser.match(TokenType.RIGHT_BRACE)) break;
+      if (parser.match(TokenType.LINE, TokenType.RIGHT_BRACE)) break;
+      
+      // Each expression is separated by a line.
+      parser.consume(TokenType.LINE);
+    }
+    
     position = position.union(parser.last(1).getPosition());
     parser.popQuote();
-    return Expr.quote(position, expr);
+    
+    if (exprs.size() == 1) return Expr.quote(position, exprs.get(0));
+    
+    return Expr.quote(position, Expr.block(exprs));
   }
 }
