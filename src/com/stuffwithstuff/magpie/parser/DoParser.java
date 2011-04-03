7aca42b23726961c0962d3d035c00126e0c4e540
Bob Nystrom <robert@stuffwithstuff.com>
2011-04-03 10:42:23 -0700

    Fix "do" parser.

--- src/com/stuffwithstuff/magpie/parser/DoParser.java
+++ src/com/stuffwithstuff/magpie/parser/DoParser.java
@@ -5,7 +5,7 @@ import com.stuffwithstuff.magpie.ast.Expr;
 public class DoParser implements PrefixParser {
   @Override
   public Expr parse(MagpieParser parser, Token token) {
-    Expr body = parser.parseExpression();
+    Expr body = parser.parseEndBlock();
     return Expr.scope(body);
   }
 }
