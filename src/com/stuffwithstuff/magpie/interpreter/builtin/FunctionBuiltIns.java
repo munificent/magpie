4f6b55fd8ee552c657951c63c010854c7cf7bc5a
Bob Nystrom <robert@stuffwithstuff.com>
2011-04-18 07:46:21 -0700

    Make function's test their patterns too.

--- src/com/stuffwithstuff/magpie/interpreter/builtin/FunctionBuiltIns.java
+++ src/com/stuffwithstuff/magpie/interpreter/builtin/FunctionBuiltIns.java
@@ -1,15 +1,28 @@
 package com.stuffwithstuff.magpie.interpreter.builtin;
 
+import com.stuffwithstuff.magpie.interpreter.Callable;
+import com.stuffwithstuff.magpie.interpreter.EvalContext;
 import com.stuffwithstuff.magpie.interpreter.FnObj;
 import com.stuffwithstuff.magpie.interpreter.Interpreter;
 import com.stuffwithstuff.magpie.interpreter.Obj;
+import com.stuffwithstuff.magpie.interpreter.PatternTester;
 
 public class FunctionBuiltIns {
   @Signature("(_ Function) call(arg)")
   public static class Call implements BuiltInCallable {
     public Obj invoke(Interpreter interpreter, Obj arg) {
       FnObj function = arg.getTupleField(0).asFn();
-      return function.invoke(interpreter, arg.getTupleField(1));
+      Obj fnArg = arg.getTupleField(1);
+      
+      // Make sure the argument matches the function's pattern.
+      Callable callable = function.getCallable();
+      EvalContext context = new EvalContext(callable.getClosure());
+      if (!PatternTester.test(interpreter, callable.getPattern(), fnArg,
+          context)) {
+        throw interpreter.error("NoMethodError");
+      }
+
+      return function.invoke(interpreter, fnArg);
     }
   }
 }
