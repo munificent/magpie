571ea03ddde6bee5b77a1420c876fcf945ab8e42
Bob Nystrom <robert@stuffwithstuff.com>
2011-06-08 08:38:05 -0700

    Work on path methods a bit.
    Renamed "base" -> "baseName".
    Start to implement "extension".

--- src/com/stuffwithstuff/magpie/intrinsic/PathMethods.java
+++ src/com/stuffwithstuff/magpie/intrinsic/PathMethods.java
@@ -8,9 +8,9 @@ import com.stuffwithstuff.magpie.interpreter.Context;
 import com.stuffwithstuff.magpie.interpreter.Obj;
 
 public class PathMethods {
-  @Def("(this is String) base")
+  @Def("(this is String) baseName")
   @Doc("Gets the base name, the filename without any leading path.")
-  public static class Base implements Intrinsic {
+  public static class BaseName implements Intrinsic {
     public Obj invoke(Context context, Obj left, Obj right) {
       return context.toObj(new File(left.asString()).getName());
     }
